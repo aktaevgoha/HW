5642d64183e5d857de97233f0feea7ad88a169b5
aktaevgoha <114522713+aktaevgoha@users.noreply.github.com>
2022-09-28 02:18:50 +0700

    Add files via upload

---
+++ HW5.py
@@ -0,0 +1,80 @@
+def greet():
+    print("добро пожаловать в игру крестики-нолики")
+    print("вводите данные в формате: Х-номер строки, У-номер столбца")
+
+def show():
+    print()
+    print("  | 0 | 1 | 2 | ")
+    print(" ---------------")
+    for i, row in enumerate(field):
+        row_str = f" {i} | {' | '.join(row)} | "
+        print(row_str)
+        print(" -------------")
+    print()
+
+def ask():
+    while True:
+        cords = input(" Ваш ход: ").split()
+
+        if len(cords) != 2:
+            print("Введите две координаты ")
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not(y.isdigit()):
+            print(" Ведите числа ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапозона ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята ")
+            continue
+
+        return x, y
+
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0")
+            return True
+    return False
+
+greet()
+field = [[""] * 3 for i in range(3)]
+count = 0
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(" ходит Х")
+    else:
+        print(" ходит 0")
+
+    x, y = ask()
+
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if count == 9:
+        print("Ничья)")
+        break
+
+
+
